a8b8e2591df24f04ba5a54469b1b1d7597fafe5c
Kritartha Ghosh <kritartha@gmail.com>
2019-03-23 15:27:39 +0800

    minor changes

--- src/main/scala/SentimentAnalysis.scala
+++ src/main/scala/SentimentAnalysis.scala
@@ -1,3 +1,6 @@
+import java.io.File
+
+import org.apache.hadoop.fs.FileUtil
 import org.apache.spark.SparkContext
 
 object SentimentAnalysis {
@@ -12,7 +15,9 @@ object SentimentAnalysis {
     val context = new SparkContext("local[*]", "SentimentAnalysis")
     //val context = SparkContext.getOrCreate()
     val lines = context.textFile("src/main/resources/tweets_2009_0.csv")
-    val stopwords = context.textFile("src/main/resources/stopwords.txt")
+    val stopWords = context.textFile("src/main/resources/stopwords.txt")
+    val outputFile = "src/main/resources/output.csv"
+    FileUtil.fullyDelete(new File(outputFile))
 
     // Removing the @words
     val parsedHash = lines.map(x => x.replaceAll(Utility.REG_HANDLERS, ""))
@@ -30,13 +35,13 @@ object SentimentAnalysis {
     val parsedContractions = parsedLowercase.map(x => x.split(" ")
       .map(y => if (Utility.contractions.contains(y)) Utility.contractions(y) else y).mkString(" "))
 
-    // Removing stopwords
-    val broadcastStopwords = context.broadcast(stopwords.collect.toSet)
-    val parsedStopwords = parsedContractions.map(x => x.split(" ")
-      .map(y => if (!broadcastStopwords.value.contains(y)) y else "").mkString(" "))
+    // Removing stopWords
+    val broadcastStopWords = context.broadcast(stopWords.collect.toSet)
+    val parsedStopWords = parsedContractions.map(x => x.split(" ")
+      .map(y => if (!broadcastStopWords.value.contains(y)) y else "").mkString(" "))
 
     // Removing extra whitespaces
-    val parsedWhitespaces = parsedStopwords.map(x => x.replaceAll(Utility.REG_WHITESPACES, " "))
+    val parsedWhitespaces = parsedStopWords.map(x => x.replaceAll(Utility.REG_WHITESPACES, " "))
 
     // Trimming the string
     val parsedTrim = parsedWhitespaces.map(x => x.replaceAll(Utility.REG_TRIM, ""))
@@ -44,10 +49,6 @@ object SentimentAnalysis {
     // Removing empty tweets
     val parsedEmpty = parsedTrim.filter(x => x.length > 0)
 
-    var count = 0
-    for (result <- parsedEmpty.collect() if count < 20) {
-      println(result)
-      count = count + 1
-    }
+    parsedEmpty.saveAsTextFile(outputFile)
   }
 }
