f2778bc977cdf306520c1e529912607e38458069
Kritartha Ghosh <kritartha@gmail.com>
2019-04-04 01:55:19 +0800

    added k fold cross validation

--- src/main/scala/SentimentModel.scala
+++ src/main/scala/SentimentModel.scala
@@ -1,7 +1,8 @@
 import org.apache.spark.ml.{Pipeline, PipelineStage}
-import org.apache.spark.ml.classification.{LinearSVC, LogisticRegression, NaiveBayes, RandomForestClassifier}
+import org.apache.spark.ml.classification._
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{HashingTF, IDF, StandardScaler, Tokenizer}
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
@@ -29,35 +30,47 @@ object SentimentModel {
       df = df.withColumn(label, when(df(label) =!= 0, 1).when(df(label) === 0, 0))
     }
 
-    val Array(trainingData, testData) = df.randomSplit(Array(0.7, 0.3))
-    var stages: Array[PipelineStage] = Array(tokenizer, hashingTF, idf, scaler)
+    val Array(trainingData, testData) = df.randomSplit(Array(0.75, 0.25))
+    val stages: Array[PipelineStage] = Array(tokenizer, hashingTF, idf, scaler)
 
+    var predicted_results: Array[String] = Array()
     for (label <- Utility.LABELS) {
       val clf = new LogisticRegression()
-        .setMaxIter(10)
-        .setRegParam(0.3)
-        .setElasticNetParam(0.8)
         .setLabelCol(label)
         .setFeaturesCol("scaledFeatures")
         .setProbabilityCol(label.concat("_prob"))
         .setRawPredictionCol(label.concat("_raw_pred"))
         .setPredictionCol(label.concat("_pred"))
 
-      stages = stages :+ clf
-    }
+      val paramMaps = new ParamGridBuilder()
+        .addGrid(hashingTF.numFeatures, Array(10, 100, 1000))
+        .addGrid(clf.regParam, Array(0.3, 0.1, 0.01))
+        .addGrid(clf.maxIter, Array(10, 20, 50))
+        .addGrid(clf.elasticNetParam, Array(0.0, 0.1, 0.2))
+        .build()
 
-    val pipeline = new Pipeline().setStages(stages)
-    val model = pipeline.fit(trainingData)
-    val predictions = model.transform(testData)
+      val updatedStages = stages :+ clf
+      val pipeline = new Pipeline().setStages(updatedStages)
 
-    var predicted_results: Array[String] = Array()
-    for (label <- Utility.LABELS) {
       val evaluator = new MulticlassClassificationEvaluator()
         .setLabelCol(label)
         .setPredictionCol(label.concat("_pred"))
         .setMetricName("accuracy")
+
+      val cv = new CrossValidator()
+        .setEstimator(pipeline)
+        .setEstimatorParamMaps(paramMaps)
+        .setEvaluator(evaluator)
+        .setNumFolds(5)
+        .setParallelism(2)
+
+      val model = cv.fit(trainingData)
+      val predictions = model.transform(testData)
+
       val accuracy = evaluator.evaluate(predictions) * 100
       predicted_results = predicted_results :+ label.concat(" accuracy " + accuracy)
+
+      println(label.concat(" accuracy " + accuracy))
     }
     predicted_results.foreach(println)
   }
